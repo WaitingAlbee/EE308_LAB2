c7054416c6b800b72e29b978c25c5ec9be98933b
WaitingAlbee <1264416178@qq.com>
2021-09-23 11:55:51 +0800

    Ignore comments or special symbols when encounter

--- CounterKeys.cpp
+++ CounterKeys.cpp
@@ -20,10 +20,19 @@ struct UserInputArg{
 struct OutputResult{
 	int keywords_num;
 	int switch_num;
-	vector<int> case_arr;
 	int if_else_num;
 	int if_elseif_else_num;
 	int level;
+	vector<int> case_arr;
+};
+
+struct IgnoreList{
+	bool double_slash; //   //
+	bool slash_star; //     /* */
+	bool quotes; //        ""
+	bool macro;  //        #
+	bool ignore_symbol_before; //当前需忽视的符号之前有无需忽视的符号
+	int quote_times; // 引号出现的次数
 };
 
 /**********************用户操作界面***************************/ 
@@ -115,14 +124,29 @@ void FileRead::closeFile(){
 class Count{
 public:
 	OutputResult out;
+	IgnoreList situation;
 	unordered_map<string, int> keywords_map; // 构建关键词哈希表，便于后续查找 
 	stack<string> ifelse_stack; // 创建ifelse栈容器，逐一判断栈顶元素 
+	Count(const string arr[], int size, int level); 
+	void isIgnoreSymbol(const string& s, int i, IgnoreList* situation);
+	void IgnoreSlash(const string& s, int i, IgnoreList* situation);
+	void IgnoreMacro(const string& s, int i, IgnoreList* situation);
+	void IgnoreQuote(const string& s, int i, IgnoreList* situation) ;
 	void countKeywords(string s);
 	void countSwitchCase(string s, int* case_arr_index);
 	bool addTopStack(string s, char* c);
 	void addBracket(char* c);
 	void countIfElse(string s, char* c);
 };
+
+Count::Count(const string arr[], int size, int level){
+	for(int i = 0; i < size; i++){
+		keywords_map.emplace(arr[i],0); // 
+	}
+	situation = { false,false,false,false,false,0 }; // 初始化 还未遇到需要忽略的符号 
+	out = { level, 0,0,0,0 }; // 初始化所有关键词数量为零 
+
+}
 
 void Count::countKeywords(string s){
 	if(keywords_map.find(s) != keywords_map.end()){ //搜索map值，找不到对应值返回0，找到对应值返回1 
@@ -144,6 +168,9 @@ void Count::countSwitchCase(string s, int* case_arr_index) {
 bool Count::addTopStack(string s, char* c){
 	if (s == "if"){
 		ifelse_stack.push("if");
+	}
+	if (s == "else"){
+		
 	}
 	return 1; 
 }
@@ -190,7 +217,59 @@ void Count::countIfElse(string s, char* c){
 	}
 }
 
+void Count::isIgnoreSymbol(const string& s, int i, IgnoreList* situation){
+	IgnoreMacro(s, i, situation);
+	IgnoreSlash(s, i, situation);
+	IgnoreQuote(s, i, situation);
+	return (situation->double_slash || situation->slash_star || situation->macro || situation->quotes);
+	
+}
 
+void Count::IgnoreSlash(const string& s, int i, IgnoreList* situation){
+	if (s[i - 1] == '/' && s[i] == '/' && !situation->ignore_symbol_before) {  //首次遇到“//” 
+		situation->double_slash = true;
+		situation->ignore_symbol_before = true;
+	}
+	if (s[i - 1] == '\n' && situation->double_slash) { // “//”后遇到换行将结束忽略 
+		situation->double_slash = false;
+		situation->ignore_symbol_before = false;
+	}
+	if (s[i - 1] == '/' && s[i] == '*' && !situation->ignore_symbol_before) {  // 首次遇到“/*” 
+		situation->slash_star = true;
+		situation->ignore_symbol_before = true;
+	}
+	if (s[i - 1] == '*' && s[i] == '/' && situation->ignore_symbol_before) {  // 遇到“*/”将结束忽略 
+		situation->slash_star = false;
+		situation->ignore_symbol_before = false;
+	}	
+}
+
+void Count::IgnoreMacro(const string& s, int i, IgnoreList* situation){
+	if (s[i - 1] == '#' && !situation->ignore_symbol_before) {  // 首次遇到“#”
+		situation->macro = true;
+		situation->ignore_symbol_before = true;
+	}
+	if (s[i - 1] == '\n' && situation->macro) { // “#”后遇到换行将结束忽略 
+		situation->macro = false;
+		situation->ignore_symbol_before = false;
+	}
+} 
+
+void Count::IgnoreQuote(const string& s, int i, IgnoreList* situation) {
+	//前面没有其他该忽略的字符 或 前面已经有一个相匹配的引号
+	if (s[i - 1] == '"' && (!situation->ignore_symbol_before || situation->quote_times == 1)){
+		situation->quotes = !situation->quotes; //计算机里引号是一样的，所以只能取反
+		
+		situation->quote_times = (situation->quote_times + 1) % 2; //奇数次结果为1，偶数次结果为0
+		if (situation->quote_times == 0) {
+			situation->ignore_symbol_before = false; // 成对引号 结束忽略 
+		}
+		else
+		{
+			situation->ignore_symbol_before = true; //  单数引号 忽略ing 
+		}
+	}
+}
 
 
 
