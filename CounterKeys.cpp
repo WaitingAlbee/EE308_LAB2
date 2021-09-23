22c471a96611fa34d4d0f5435b4fa9a2c268ce35
WaitingAlbee <1264416178@qq.com>
2021-09-23 12:35:01 +0800

    count work

--- CounterKeys.cpp
+++ CounterKeys.cpp
@@ -127,8 +127,9 @@ public:
 	IgnoreList situation;
 	unordered_map<string, int> keywords_map; // 构建关键词哈希表，便于后续查找 
 	stack<string> ifelse_stack; // 创建ifelse栈容器，逐一判断栈顶元素 
-	Count(const string arr[], int size, int level); 
-	void isIgnoreSymbol(const string& s, int i, IgnoreList* situation);
+	
+	void GetReady(const string arr[], int size, int level);
+	bool isIgnoreSymbol(const string& s, int i, IgnoreList* situation);
 	void IgnoreSlash(const string& s, int i, IgnoreList* situation);
 	void IgnoreMacro(const string& s, int i, IgnoreList* situation);
 	void IgnoreQuote(const string& s, int i, IgnoreList* situation) ;
@@ -137,15 +138,70 @@ public:
 	bool addTopStack(string s, char* c);
 	void addBracket(char* c);
 	void countIfElse(string s, char* c);
+	void countWork(string content, int level);
 };
 
-Count::Count(const string arr[], int size, int level){
+void Count::GetReady(const string arr[], int size, int level){
 	for(int i = 0; i < size; i++){
 		keywords_map.emplace(arr[i],0); // 
 	}
 	situation = { false,false,false,false,false,0 }; // 初始化 还未遇到需要忽略的符号 
 	out = { level, 0,0,0,0 }; // 初始化所有关键词数量为零 
 
+}
+
+bool Count::isIgnoreSymbol(const string& s, int i, IgnoreList* situation){
+	IgnoreMacro(s, i, situation);
+	IgnoreSlash(s, i, situation);
+	IgnoreQuote(s, i, situation);
+	return (situation->double_slash || situation->slash_star || situation->macro || situation->quotes);
+	
+}
+
+void Count::IgnoreSlash(const string& s, int i, IgnoreList* situation){
+	if (s[i - 1] == '/' && s[i] == '/' && !situation->ignore_symbol_before) {  //首次遇到“//” 
+		situation->double_slash = true;
+		situation->ignore_symbol_before = true;
+	}
+	if (s[i - 1] == '\n' && situation->double_slash) { // “//”后遇到换行将结束忽略 
+		situation->double_slash = false;
+		situation->ignore_symbol_before = false;
+	}
+	if (s[i - 1] == '/' && s[i] == '*' && !situation->ignore_symbol_before) {  // 首次遇到“/*” 
+		situation->slash_star = true;
+		situation->ignore_symbol_before = true;
+	}
+	if (s[i - 1] == '*' && s[i] == '/' && situation->ignore_symbol_before) {  // 遇到“*/”将结束忽略 
+		situation->slash_star = false;
+		situation->ignore_symbol_before = false;
+	}	
+}
+
+void Count::IgnoreMacro(const string& s, int i, IgnoreList* situation){
+	if (s[i - 1] == '#' && !situation->ignore_symbol_before) {  // 首次遇到“#”
+		situation->macro = true;
+		situation->ignore_symbol_before = true;
+	}
+	if (s[i - 1] == '\n' && situation->macro) { // “#”后遇到换行将结束忽略 
+		situation->macro = false;
+		situation->ignore_symbol_before = false;
+	}
+} 
+
+void Count::IgnoreQuote(const string& s, int i, IgnoreList* situation) {
+	//前面没有其他该忽略的字符 或 前面已经有一个相匹配的引号
+	if (s[i - 1] == '"' && (!situation->ignore_symbol_before || situation->quote_times == 1)){
+		situation->quotes = !situation->quotes; //计算机里引号是一样的，所以只能取反
+		
+		situation->quote_times = (situation->quote_times + 1) % 2; //奇数次结果为1，偶数次结果为0
+		if (situation->quote_times == 0) {
+			situation->ignore_symbol_before = false; // 成对引号 结束忽略 
+		}
+		else
+		{
+			situation->ignore_symbol_before = true; //  单数引号 忽略ing 
+		}
+	}
 }
 
 void Count::countKeywords(string s){
@@ -157,7 +213,7 @@ void Count::countKeywords(string s){
 void Count::countSwitchCase(string s, int* case_arr_index) {
 	if (s == "switch") {
 		out.switch_num++;
-		out.case_arr.push_back(0); // 在数组后添加一项 （创建空间） 
+		out.case_arr.push_back(0); // 在数组后添加一项 （创建空间开始存放case数量） 
 		(*case_arr_index)++;
 	}
 	else if (s == "case") {
@@ -217,59 +273,33 @@ void Count::countIfElse(string s, char* c){
 	}
 }
 
-void Count::isIgnoreSymbol(const string& s, int i, IgnoreList* situation){
-	IgnoreMacro(s, i, situation);
-	IgnoreSlash(s, i, situation);
-	IgnoreQuote(s, i, situation);
-	return (situation->double_slash || situation->slash_star || situation->macro || situation->quotes);
+void Count::countWork(string content, int level){
+	int index = 0;
+	int switch_num_temp = 0;
+	int case_arr_index = -1;
+	string word;
 	
-}
-
-void Count::IgnoreSlash(const string& s, int i, IgnoreList* situation){
-	if (s[i - 1] == '/' && s[i] == '/' && !situation->ignore_symbol_before) {  //首次遇到“//” 
-		situation->double_slash = true;
-		situation->ignore_symbol_before = true;
-	}
-	if (s[i - 1] == '\n' && situation->double_slash) { // “//”后遇到换行将结束忽略 
-		situation->double_slash = false;
-		situation->ignore_symbol_before = false;
-	}
-	if (s[i - 1] == '/' && s[i] == '*' && !situation->ignore_symbol_before) {  // 首次遇到“/*” 
-		situation->slash_star = true;
-		situation->ignore_symbol_before = true;
-	}
-	if (s[i - 1] == '*' && s[i] == '/' && situation->ignore_symbol_before) {  // 遇到“*/”将结束忽略 
-		situation->slash_star = false;
-		situation->ignore_symbol_before = false;
-	}	
-}
-
-void Count::IgnoreMacro(const string& s, int i, IgnoreList* situation){
-	if (s[i - 1] == '#' && !situation->ignore_symbol_before) {  // 首次遇到“#”
-		situation->macro = true;
-		situation->ignore_symbol_before = true;
-	}
-	if (s[i - 1] == '\n' && situation->macro) { // “#”后遇到换行将结束忽略 
-		situation->macro = false;
-		situation->ignore_symbol_before = false;
-	}
-} 
-
-void Count::IgnoreQuote(const string& s, int i, IgnoreList* situation) {
-	//前面没有其他该忽略的字符 或 前面已经有一个相匹配的引号
-	if (s[i - 1] == '"' && (!situation->ignore_symbol_before || situation->quote_times == 1)){
-		situation->quotes = !situation->quotes; //计算机里引号是一样的，所以只能取反
-		
-		situation->quote_times = (situation->quote_times + 1) % 2; //奇数次结果为1，偶数次结果为0
-		if (situation->quote_times == 0) {
-			situation->ignore_symbol_before = false; // 成对引号 结束忽略 
+	for (int i = 1; i < content.size(); i++){
+		if(isIgnoreSymbol(content, i, &situation)){
+			continue; // 处理所有需要忽略的情况 
+		}		
+		if (!isalpha(content[i - 1]) && isalpha(content[i])) {
+			index = i;
 		}
-		else
-		{
-			situation->ignore_symbol_before = true; //  单数引号 忽略ing 
+		if (isalpha(content[i - 1]) && !isalpha(content[i])) {   
+			word = content.substr(index, i - index); // 根据判断是否为英文字母提取单词 E.g. /nif(){} 提取“if” 
+			
+			countKeywords(word);
+			countSwitchCase(word, &case_arr_index);
+			countIfElse(word, &content[i]);
 		}
+		addBracket(&content[i - 1]);
 	}
 }
+
+
+
+
 
 
 
